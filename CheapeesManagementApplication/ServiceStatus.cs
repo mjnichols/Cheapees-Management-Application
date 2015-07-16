2ef070ce291ea0112bfcea1365edf9c72879e1f6
Michael Nichols <senseofnickels@outlook.com>
2015-07-16 18:33:21 -0500

    Initial commit - working on views.

---
+++ CheapeesManagementApplication/ServiceStatus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheapeesManagementApplication
+{
+  class ServiceStatus
+  {
+    String serviceName { get; set; }
+    Status status { get; set; }
+    String errorMessage { get; set; }
+    DateTime lastRun { get; set; }
+    DateTime nextRun { get; set; }
+
+    public ServiceStatus()
+    {
+
+    }
+  }
+
+  enum Status { Success, Error, Disabled, NotYetRun }
+}
